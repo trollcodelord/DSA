106b670587ce0ab40392efdfadbf2f87f0173c00
trollcodelord <k.bhupenderagile@gmail.com>
2021-12-01 13:33:32 +0530

    leetcode1365

---
+++ src/Arrays/LeetCode1365.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class LeetCode1365 {
+
+	
+	 public int[] smallerNumbersThanCurrent(int[] nums) {
+	     
+	     int arr[] = new int[nums.length];
+	        
+	        for(int i=0;i<nums.length;i++){
+	            int count = 0;
+	            for(int j=0; j<nums.length;j++){
+	                
+	                
+	                if(nums[i]>nums[j] && j!=i)
+	                {
+	                  count++;
+	                }
+	            }
+	            arr[i] = count;
+	        }
+	        
+	        return arr;
+	    }
+	}
