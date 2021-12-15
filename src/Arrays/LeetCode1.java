ca56eba6d42ab09a694d1f9b39f9ab624686db54
trollcodelord <k.bhupenderagile@gmail.com>
2021-12-15 14:58:46 +0530

    LeetCode1

---
+++ src/Arrays/LeetCode1.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.util.HashMap;
+
+public class LeetCode1 {
+
+	
+	 public int[] twoSum(int[] nums, int target) {
+	        
+	        
+	        int[] ans = new int[2];
+	        
+	        HashMap<Integer, Integer> map = new HashMap<>();
+	        
+	        for(int i=0; i<nums.length; i++){
+	            
+	            if(map.containsKey(target-nums[i])){
+	                ans[0] =i;
+	                ans[1] = map.get(target-nums[i]);
+	                break;
+	                
+	            }else{
+	                
+	                map.put(nums[i],i);
+	            }
+	        }
+	        return ans;
+	    }
+	}
+
