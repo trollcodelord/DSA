79436ad0cf05e872439f3fd61bd5d8ec708637e4
trollcodelord <k.bhupenderagile@gmail.com>
2022-02-07 01:02:23 +0530

    Find Duplicate in Array II

---
+++ src/febLeetCode22/FebSixth.java
@@ -0,0 +1,17 @@
+package febLeetCode22;
+
+public class FebSixth {
+
+	public int removeDuplicates(int[] nums) {
+		
+		int i =0;
+		
+		for(int e  : nums) {
+			if( i == 0 || i == 1 ||  nums[i-2] != e)
+				nums[i] = e;
+			i++;
+			
+		}
+	return i;	
+	}
+}
